4cfd189ebab0ea1071fdc2b120b015fe83feb437
lgh7418 <lgh7418@gmial.com>
2019-05-22 22:14:34 +0900

    비즈니스 계층 CRUD 구현

--- src/main/java/com/company/config/RootConfig.java
+++ src/main/java/com/company/config/RootConfig.java
@@ -7,12 +7,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@ComponentScan(basePackages="com.company.service")
 @MapperScan(basePackages= {"com.company.mapper"})
 public class RootConfig {
 	
---
+++ src/test/java/com/company/service/BoardServiceTest.java
@@ -0,0 +1,78 @@
+package com.company.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.company.config.RootConfig;
+import com.company.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Log4j
+public class BoardServiceTest {
+	
+	@Setter(onMethod_ = { @Autowired })
+	private BoardService service;
+
+	//@Test
+	public void testExist() {
+
+		log.info(service);
+		assertNotNull(service);
+	}
+
+	//@Test
+	public void testRegister() {
+
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+
+		service.register(board);
+		
+		// mapper의 insertSelectKey() 결과로 board에 bno를 설정하므로 bno를 확인할 수 있음
+		log.info("생성된 게시물의 번호: " + board.getBno());
+	}
+	
+	//@Test
+	public void testGetList() {
+
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	//@Test
+	public void testGet() {
+
+		log.info(service.get(1L));
+	}
+	
+	//@Test
+	public void testUpdate() {
+
+		BoardVO board = service.get(23L);
+
+		if (board == null) {
+			return;
+		}
+
+		board.setTitle("제목 수정합니다.");
+		log.info("MODIFY RESULT: " + service.modify(board));
+	}
+	
+	@Test
+	public void testDelete() {
+
+		log.info("REMOVE RESULT: " + service.remove(23L));
+
+	}
+
+}
